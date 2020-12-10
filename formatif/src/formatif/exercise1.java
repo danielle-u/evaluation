e0c856c4e2f6b0ad574b9badf423ac639bd00f67
dani <dani@localhost>
2020-12-10 11:59:59 -0500

    premier

---
+++ formatif/src/formatif/exercise1.java
@@ -0,0 +1,15 @@
+package formatif;
+/*
+* Author : dani
+*Date: Dec. 10, 2020
+*/
+public class exercise1
+	{
+
+		public static void main(String[] args)
+			{
+				
+
+			}
+
+	}
---
+++ formatif/src/formatif/exercise3.java
@@ -0,0 +1,16 @@
+package formatif;
+/*
+* Author : dani
+*Date: Dec. 10, 2020
+*/
+public class exercise3
+	{
+
+		public static void main(String[] args)
+			{
+				int []s= {12, 45, 33, 11};
+				methodestableaux.plusGrandIndex(s);
+
+			}
+
+	}
